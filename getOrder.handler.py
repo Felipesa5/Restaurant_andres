40e265e5621777956ff14f26220108321a5cdd49
Andres Felipe Sanchez M <118855292+Felipesa5@users.noreply.github.com>
2023-07-11 23:20:40 -0500

    Update getOrder.handler.py

--- getOrder.handler.py
+++ getOrder.handler.py
@@ -2,27 +2,27 @@ import json
 import mysql.connector
 
 def handler(event, context):
-    # Obtener el orderId del evento recibido
+   
     orderId = event['pathParameters']['orderId']
 
-    # Obtener el pedido de la base de datos
+  
     pedido = obtener_pedido(orderId)
 
     if pedido is not None:
-        # Pedido encontrado
+        
         return {
             'statusCode': 200,
             'body': json.dumps(pedido)
         }
     else:
-        # Pedido no encontrado
+      
         return {
             'statusCode': 404,
             'body': json.dumps({'message': 'Pedido no encontrado'})
         }
 
 def obtener_pedido(orderId):
-    # Conexión a la base de datos
+   
     connection = mysql.connector.connect(
         host='andres-restaurant-dev-mydatabase-zibgtqarvqcq.c2qhdzf7irsz.us-east-1.rds.amazonaws.com',
         database='andres-restaurant',
@@ -30,16 +30,16 @@ def obtener_pedido(orderId):
         password='123456789'
     )
 
-    # Obtener el pedido de la tabla "pedido" usando el orderId
+    
     cursor = connection.cursor()
     query = "SELECT * FROM pedido WHERE orderId = %s"
     values = (orderId,)
     cursor.execute(query, values)
 
-    # Obtener el resultado del pedido
+   
     pedido = cursor.fetchone()
 
-    # Cerrar cursor y conexión
+    
     cursor.close()
     connection.close()
 
