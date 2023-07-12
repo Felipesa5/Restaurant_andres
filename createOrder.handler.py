eb513d6df45b3e7e0c8800f1981c830276247352
Andres Felipe Sanchez M <118855292+Felipesa5@users.noreply.github.com>
2023-07-11 23:22:20 -0500

    Update createOrder.handler.py

--- createOrder.handler.py
+++ createOrder.handler.py
@@ -4,47 +4,45 @@ import boto3
 ses = boto3.client('ses')
 
 def handler(event):
-    # Obtener los datos del pedido del evento recibido
-
-    # Crear una instancia del servicio SQS
+   
     sqs = boto3.client('sqs')
 
-    # Configurar los parámetros del mensaje
+  
     params = {
-        'MessageBody': json.dumps(event)  # Agrega los datos del pedido aquí
+        'MessageBody': json.dumps(event) 
     }
 
     try:
-        # Enviar el mensaje a la cola
+       
         response = sqs.send_message(QueueUrl='https://sqs.us-east-1.amazonaws.com/101626206766/order-queue', MessageBody=params['MessageBody'])
 
-        # Enviar el correo electrónico al cliente
+      
         emailParams = {
             'Destination': {
-                'ToAddresses': ['pl8973166gmail.com']  # Reemplaza con la dirección de correo electrónico del cliente
+                'ToAddresses': ['pl8973166gmail.com']  
             },
             'Message': {
                 'Body': {
                     'Text': {
-                        'Data': '¡Gracias por tu pedido!'  # Reemplaza con el cuerpo del mensaje que deseas enviar
+                        'Data': '¡Gracias por tu pedido!'  
                     }
                 },
                 'Subject': {
-                    'Data': 'Confirmación de pedido'  # Reemplaza con el asunto del correo electrónico
+                    'Data': 'Confirmación de pedido' 
                 }
             },
-            'Source': 'morenofelipe513@gmail.com'  # Reemplaza con tu dirección de correo electrónico
+            'Source': 'morenofelipe513@gmail.com' 
         }
 
         response = ses.send_email(**emailParams)
 
-        # Retornar la respuesta al cliente
+      
         return {
             'statusCode': 200,
             'body': json.dumps({'message': 'Pedido creado exitosamente'})
         }
     except Exception as error:
-        # En caso de error, manejarlo adecuadamente
+       
         return {
             'statusCode': 500,
             'body': json.dumps({'message': 'Error al crear el pedido'})
@@ -55,13 +53,11 @@ def handler(event):
 
 
 def handler(event):
-    # Obtener los datos del pedido del evento recibido
+  
     order_data = event['body']  # Se asume que los datos del pedido se encuentran en el campo 'body' del evento
 
-    # Realizar la lógica para crear el pedido en la base de datos
-    # Aquí puedes agregar tu código para crear el pedido en la base de datos
-
-    # Retornar la respuesta al cliente
+  
+   
     return {
         'statusCode': 200,
         'body': json.dumps({'message': 'Pedido creado exitosamente'})
