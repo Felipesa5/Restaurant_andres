c5d5ac96e426fa0f7995ff95db5a07dd8ee32d38
Andres Felipe Sanchez M <118855292+Felipesa5@users.noreply.github.com>
2023-07-11 23:20:11 -0500

    Update handler.lambda_handler.py

--- handler.lambda_handler.py
+++ handler.lambda_handler.py
@@ -1,7 +1,7 @@
 import pymysql
 
 def lambda_handler(event, context):
-    # Configura la conexión a MySQL
+    
     connection = pymysql.connect(
         host='andres-restaurant-dev-mydatabase-zibgtqarvqcq.c2qhdzf7irsz.us-east-1.rds.amazonaws.com',
         user='admin',
@@ -11,14 +11,14 @@ def lambda_handler(event, context):
     
     try:
         with connection.cursor() as cursor:
-            # Realiza una consulta
+            
             sql = "SELECT * FROM tu_tabla"
             cursor.execute(sql)
             result = cursor.fetchall()
             
-            # Imprime los resultados
+           
             for row in result:
                 print(row)
     finally:
-        # Cierra la conexión a MySQL
+        
         connection.close()
