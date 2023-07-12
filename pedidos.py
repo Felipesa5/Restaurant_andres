af7c9eb5f36db1bba9492f608011481061ce8095
Andres Felipe Sanchez M <118855292+Felipesa5@users.noreply.github.com>
2023-07-11 23:19:08 -0500

    Update pedidos.py

--- pedidos.py
+++ pedidos.py
@@ -1,5 +1,5 @@
 def insertar_cliente(nombre, cedula, direccion, correo):
-    # Obtener información de conexión desde variables de entorno o alguna fuente segura
+    
     connection_info = {
         'host': 'andres-restaurant-dev-mydatabase-zibgtqarvqcq.c2qhdzf7irsz.us-east-1.rds.amazonaws.com',
         'database': 'andres-restaurant',
@@ -7,18 +7,18 @@ def insertar_cliente(nombre, cedula, direccion, correo):
         'password': '123456789'
     }
 
-    # Conexión a la base de datos utilizando un administrador de contexto
+    # Conexión a la base de datos 
     with mysql.connector.connect(**connection_info) as connection:
-        # Inserción de datos en la tabla "cliente"
+        
         cursor = connection.cursor()
         query = "INSERT INTO cliente (nombre, cedula, direccion, correo) VALUES (%s, %s, %s, %s)"
         values = (nombre, cedula, direccion, correo)
         cursor.execute(query, values)
 
-        # Confirmar cambios (no es necesario cerrar explícitamente el cursor o la conexión)
+        
 
 def insertar_pedido(producto, cantidad, valor_unitario):
-    # Obtener información de conexión desde variables de entorno o alguna fuente segura
+    
     connection_info = {
         'host': 'andres-restaurant-dev-mydatabase-zibgtqarvqcq.c2qhdzf7irsz.us-east-1.rds.amazonaws.com',
         'database': 'andres-restaurant',
@@ -26,12 +26,12 @@ def insertar_pedido(producto, cantidad, valor_unitario):
         'password': '123456789'
     }
 
-    # Conexión a la base de datos utilizando un administrador de contexto
+    
     with mysql.connector.connect(**connection_info) as connection:
-        # Inserción de datos en la tabla "pedido"
+        
         cursor = connection.cursor()
         query = "INSERT INTO pedido (producto, cantidad, valor_unitario) VALUES (%s, %s, %s)"
         values = (producto, cantidad, valor_unitario)
         cursor.execute(query, values)
 
-        # Confirmar cambios (no es necesario cerrar explícitamente el cursor o la conexión)
+        
